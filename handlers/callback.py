f7c8cbb48983bf12d40def03e5a366579f9dad20
adelina <141991621+delinwy@users.noreply.github.com>
2023-11-07 23:52:44 +0600

    8th hw preparation for deploy

--- handlers/callback.py
+++ handlers/callback.py
@@ -4,7 +4,7 @@ from aiogram import types, Dispatcher
 from config import bot
 from keyboards.inline_buttons import questionnaire_one_keyboard
 from database.sql_commands import Database
-from scraping.news_scraper import NewsScraper
+# from scraping.news_scraper import NewsScraper
 
 
 async def start_questionnaire(call: types.CallbackQuery):
@@ -52,14 +52,14 @@ async def no_answer(call: types.CallbackQuery):
     )
 
 
-async def latest_news_call(call: types.CallbackQuery):
-    scraper = NewsScraper()
-    links = scraper.parse_data()
-    for link in links:
-        await bot.send_message(
-            chat_id=call.message.chat.id,
-            text=scraper.PLUS_URL + link,
-        )
+# async def latest_news_call(call: types.CallbackQuery):
+#     scraper = NewsScraper()
+#     links = scraper.parse_data()
+#     for link in links:
+#         await bot.send_message(
+#             chat_id=call.message.chat.id,
+#             text=scraper.PLUS_URL + link,
+#         )
 
 
 def register_callback_handlers(dp: Dispatcher):
@@ -69,5 +69,5 @@ def register_callback_handlers(dp: Dispatcher):
                                        lambda call: call.data == 'okay_yes')
     dp.register_callback_query_handler(no_answer,
                                        lambda call: call.data == 'okay_no')
-    dp.register_callback_query_handler(latest_news_call,
-                                       lambda call: call.data == 'latest_news')
+    # dp.register_callback_query_handler(latest_news_call,
+    #                                    lambda call: call.data == 'latest_news')
--- scraping/news_scraper.py
+++ scraping/news_scraper.py
@@ -1,22 +1,22 @@
-from parsel import Selector
-import requests
-
-
-class NewsScraper:
-    URL = 'https://www.riotgames.com/en/news'
-    LINK_XPATH = '//div[@class="summary"]/a/@href'
-    PLUS_URL = 'https://www.riotgames.com'
-
-    def parse_data(self):
-        html = requests.get(url=self.URL).text
-        tree = Selector(text=html)
-        links = tree.xpath(self.LINK_XPATH).extract()
-        for link in links:
-            print(self.PLUS_URL + link)
-
-        return links[:5]
-
-
-if __name__ == '__main__':
-    scraper = NewsScraper()
-    scraper.parse_data()
+# from parsel import Selector
+# import requests
+#
+#
+# class NewsScraper:
+#     URL = 'https://www.riotgames.com/en/news'
+#     LINK_XPATH = '//div[@class="summary"]/a/@href'
+#     PLUS_URL = 'https://www.riotgames.com'
+#
+#     def parse_data(self):
+#         html = requests.get(url=self.URL).text
+#         tree = Selector(text=html)
+#         links = tree.xpath(self.LINK_XPATH).extract()
+#         for link in links:
+#             print(self.PLUS_URL + link)
+#
+#         return links[:5]
+#
+#
+# if __name__ == '__main__':
+#     scraper = NewsScraper()
+#     scraper.parse_data()
